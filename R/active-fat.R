df22af58dc98c7810226f3240f90b52d3cdde3a5
rtakasuke <takahashi@tamagohan.skr.jp>
2021-03-07 20:06:25 +0900

    体脂肪率 -> 体脂肪量
    fix

--- R/active-fat.R
+++ R/active-fat.R
@@ -1,26 +1,41 @@
-# 運動量と翌日の体脂肪率増減の関係を解析
-
-# Body fat : 体脂肪率 [%]
-filter.type <- "HKQuantityTypeIdentifierBodyFatPercentage"
-filter.source <- "HealthPlanet"
-df.bodyfat <- df %>%
-    filter(type == filter.type & sourceName == filter.source) %>%
-    select(date, value) %>%
-    mutate_at(vars(value), as.numeric) %>%
-    rename(bodyfat = value) %>%
-    mutate(bodyfat = bodyfat * 100) %>%             # 百分率に変換
-    mutate(bodyfat.diff = lead(bodyfat) - bodyfat)  # 次回計測時の減少量
+# 運動量と翌日の体脂肪増減の関係を解析
 
 # Active energy : アクティブエネルギー [kcal]
 filter.type <- "HKQuantityTypeIdentifierActiveEnergyBurned"
 filter.source <- "Mi Fit"
 df.activeenergy <- df %>%
     filter(type == filter.type & sourceName == filter.source) %>%
-    select(date, value) %>%
+    select(lifedate, value) %>%
     mutate_at(vars(value), as.numeric) %>%
+    rename(date = lifedate) %>%
     rename(activeenergy = value) %>%
     group_by(date) %>%
     summarise(activeenergy = sum(activeenergy))
+
+# Weight [kg]
+filter.type <- "HKQuantityTypeIdentifierBodyMass"
+filter.source <- "HealthPlanet"
+df.weight <- df %>%
+    filter(type == filter.type & sourceName == filter.source) %>%
+    select(date, value) %>%
+    mutate_at(vars(value), as.numeric) %>%
+    rename(weight = value)
+
+# Body fat rate : 体脂肪率
+filter.type <- "HKQuantityTypeIdentifierBodyFatPercentage"
+filter.source <- "HealthPlanet"
+df.bodyfat.rate <- df %>%
+    filter(type == filter.type & sourceName == filter.source) %>%
+    select(date, value) %>%
+    mutate_at(vars(value), as.numeric) %>%
+    rename(bodyfat.rate = value)
+
+# Body fat : 体脂肪量 [kg] = `Wight` * `Body fat rate`
+df.bodyfat <- df.bodyfat.rate %>%
+    merge(df.weight, "date", all = T) %>%
+    group_by(date) %>%
+    summarise(bodyfat = bodyfat.rate * weight) %>%
+    mutate(bodyfat.diff = lead(bodyfat) - bodyfat)  # 次回計測時の減少量
 
 # 日付を key にして join
 df.join <- df.bodyfat %>%
@@ -42,7 +57,7 @@ plot(x, y,
      ylim = c(-y.abs.max, y.abs.max),   # Y 軸の中心を0にする
      main = "Reration of Active energy and next day's Body fat",
      xlab = "Active energy [kcal]",
-     ylab = "Body fat difference [%]",
+     ylab = "Increased body fat [kg]",
      pch = 16,                          # ●でプロット
      col = ifelse(y > 0,'red','blue'))
 abline(h = 0)                           # Y = 0 に線を引く
